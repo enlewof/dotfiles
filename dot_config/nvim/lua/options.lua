e081a2ba0bf48229d0b23bd1d5022e0c5035ba77
Allen Lew <enlewof@gmail.com>
2024-04-01 00:09:37 -0700

    feat: add more format options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,3 +1,10 @@
--- Vim Options
+-- Cursor Options
 
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
+
+-- tabs, indentating, and backspace
+vim.opt.expandtab = true  -- spaces instead of tabs
+vim.opt.tabstop = 2       -- 2 spaces for tabs
+vim.opt.shiftwidth = 2    -- 2 spaces for indentation
+vim.opt.backspace = 2     -- backspace fix after plugin installs
+
