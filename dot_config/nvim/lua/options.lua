ba96045b6055004506dfb1373d26d583aea54118
Allen Lew <enlewof@gmail.com>
2024-04-01 00:12:15 -0700

    fix: remove backspace option

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -6,5 +6,4 @@ vim.opt.guicursor = "n-v-i-c:block-Cursor"
 vim.opt.expandtab = true  -- spaces instead of tabs
 vim.opt.tabstop = 2       -- 2 spaces for tabs
 vim.opt.shiftwidth = 2    -- 2 spaces for indentation
-vim.opt.backspace = 2     -- backspace fix after plugin installs
 
