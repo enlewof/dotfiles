fc98ce22712c4ed483db84022b513c3bf04c6430
Allen Lew <alew@magnite.com>
2024-04-01 18:58:39 -0700

    feature() [none]: switch to treesitter for synatax, keep the nagios one

--- dot_config/nvim/lua/plugins/syntax.lua
+++ dot_config/nvim/lua/plugins/syntax.lua
@@ -2,6 +2,24 @@
 
 return {
   'crazy-canux/nagios.vim',
-  'rodjek/vim-puppet',
-  'hashivim/vim-terraform',
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    -- configure treesitter
+    treesitter.setup({ -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "lua",
+        "puppet",
+        "terraform"
+      },
+    })
+  end,
 }
