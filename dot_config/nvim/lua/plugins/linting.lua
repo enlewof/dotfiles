f1aae5175e4961ac2c0d72bfac4ae121540f2b2a
Allen Lew <alew@magnite.com>
2024-04-01 22:45:45 -0700

    feature() [none]: change to nvim-lint

--- dot_config/nvim/lua/plugins/linting.lua
+++ dot_config/nvim/lua/plugins/linting.lua
@@ -1,5 +1,34 @@
 -- Install linting related plugins
 
 return {
-  'dense-analysis/ale',
+  "mfussenegger/nvim-lint",
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      lua = { "luacheck" },
+      puppet = { "puppet-lint" },
+      python = { "pylint" },
+      terraform = { "tflint" },
+      yaml = { "yamllint" },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
 }
