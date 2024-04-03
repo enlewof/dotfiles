5fd271fff358557e41f34277ad9d95a75863878e
Allen Lew <alew@magnite.com>
2024-04-02 21:51:30 -0700

    feature() [none]: format file

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -2,30 +2,30 @@
 -- From https://www.josean.com/posts/neovim-linting-and-formatting
 return {
   {"williamboman/mason.nvim",
-  config = function()
-    -- import mason
-    local mason = require("mason")
+    config = function()
+      -- import mason
+      local mason = require("mason")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
+      -- enable mason and configure icons
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
         },
-      },
-    })
-  end,},
+      })
+    end,},
   {"williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup {
-      ensure_installed = { "lua_ls", "terraformls" },
-    }
-  end,},
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = { "lua_ls", "terraformls" },
+      }
+    end,},
   {"neovim/nvim-lspconfig",
-  config = function()
-    require'lspconfig'.lua_ls.setup{}
-    require'lspconfig'.terraformls.setup{}
-  end,},
+    config = function()
+      require'lspconfig'.lua_ls.setup{}
+      require'lspconfig'.terraformls.setup{}
+    end,},
 }
