5646ba5eafe929092fdca49f647d0371847f4fdf
Allen Lew <alew@magnite.com>
2024-07-03 18:29:00 -0700

    feature() [none]: switch back to esqueleto

--- dot_config/nvim/lua/plugins/templates.lua
+++ dot_config/nvim/lua/plugins/templates.lua
@@ -1,19 +1,10 @@
 return {
-  'Futarimiti/spooky.nvim',
+  'cvigilv/esqueleto.nvim',
   config = function()
-    require('spooky').setup { directory = vim.fn.stdpath('config') .. '/skeletons'
-      , case_sensitive = false
-      , auto_use_only = true
-      , ui = { select = 'builtin'
-        , select_full_path = false
-        , show_no_template = true
-        , prompt = 'Select template'
-        , previewer_prompt = 'Preview'
-        , preview_normalised = true
-        , highlight_preview = true
-        , no_template = '<No template>'
-        , telescope_opts = {}
+    require("esqueleto").setup(
+      {
+        patterns = { "terraform" },
       }
-    }
+    )
   end,
 }
