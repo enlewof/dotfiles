ef27c009f67855ed8dbefa1e79230f67d251468d
Allen Lew <alew@magnite.com>
2024-07-03 18:14:51 -0700

    fix() [none]: add setup code for spooky

--- dot_config/nvim/lua/plugins/templates.lua
+++ dot_config/nvim/lua/plugins/templates.lua
@@ -1,3 +1,19 @@
 return {
   'Futarimiti/spooky.nvim',
+  config = function()
+    require('spooky').setup { directory = vim.fn.stdpath('config') .. '/skeletons'
+      , case_sensitive = false
+      , auto_use_only = true
+      , ui = { select = 'builtin'
+        , select_full_path = false
+        , show_no_template = true
+        , prompt = 'Select template'
+        , previewer_prompt = 'Preview'
+        , preview_normalised = true
+        , highlight_preview = true
+        , no_template = '<No template>'
+        , telescope_opts = {}
+      }
+    }
+  end,
 }
