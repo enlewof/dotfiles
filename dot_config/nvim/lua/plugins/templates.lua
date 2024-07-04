4c48235132373c66204cbf2bc520d1ecb855f0a7
Allen Lew <alew@magnite.com>
2024-07-03 18:54:35 -0700

    feature() [none]: settled on skel-nvim for now

--- dot_config/nvim/lua/plugins/templates.lua
+++ dot_config/nvim/lua/plugins/templates.lua
@@ -1,10 +1,11 @@
 return {
-  'cvigilv/esqueleto.nvim',
+  'motosir/skel-nvim',
   config = function()
-    require("esqueleto").setup(
-      {
-        patterns = { "terraform" },
+    require("skel-nvim").setup{
+      -- file pattern -> template mappings
+      mappings = {
+        ['s3.tf'] = "s3.tf.skel",
       }
-    )
+    }
   end,
 }
