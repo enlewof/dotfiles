bc051adc15793bbce64443b794ae9aa854bdac83
Allen Lew <alew@magnite.com>
2025-05-14 16:34:04 -0700

    feature() [none]: add formatter plugin

---
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,20 @@
+-- Formatter configuration for Neovim
+
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+	end,
+}
