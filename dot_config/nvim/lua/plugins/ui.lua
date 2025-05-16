f6b654c2477b3ad2ac88e731a4487b46e099c74c
Allen Lew <alew@magnite.com>
2025-05-15 18:23:05 -0700

    feature() [none]: add trouble.nvim for diagonstics/errors

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -5,15 +5,6 @@
 -- Noice command line
 return {
 	{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
-	{
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("lualine").setup({
-				options = { theme = "tokyonight" },
-			})
-		end,
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
 	{ "nvim-telescope/telescope.nvim" },
 	{
 		"folke/noice.nvim",
@@ -46,5 +37,51 @@ return {
 			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = { theme = "tokyonight" },
+			})
+		end,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 }
