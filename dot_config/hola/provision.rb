a236ea25b2ed399920c9cfce0124149cf239c034
Allen Lew <alew@magnite.com>
2026-04-14 22:10:12 -0700

    feature() [none]: remove nix darwin configs; add hola dock provision

---
+++ dot_config/hola/provision.rb
@@ -0,0 +1,14 @@
+macos_dock do
+  apps [
+    '/Applications/Brave Browser.app/',
+    '/Applications/Slack.app/',
+    '/Applications/iTerm.app/',
+    '/Applications/Tailscale.app/',
+    '/Applications/1Password.app/',
+  ]
+  orientation :bottom
+  autohide true
+  magnification false
+  tilesize 50
+  largesize 64
+end
