8dad9927fe8d5763c37aac655bf99ba0337449eb
Allen Lew <enlewof@gmail.com>
2026-05-03 12:24:42 -0700

    add provision.rb for home

---
+++ .config/hola/provision.rb
@@ -0,0 +1,13 @@
+macos_dock do
+  apps [
+    '/Applications/Brave Browser.app/',
+    '/Applications/iTerm.app/',
+    '/Applications/Tailscale.app/',
+    '/Applications/Bitwarden.app/',
+  ]
+  orientation :bottom
+  autohide true
+  magnification false
+  tilesize 50
+  largesize 64
+end
